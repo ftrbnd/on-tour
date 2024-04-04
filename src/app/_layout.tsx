e51c57bc6446fcc247b5b0434505e977ce38a9bf
ftrbnd <giosalas25@gmail.com>
2024-04-04 12:44:03 -0700

    Add react-query storage persister

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,25 @@
 import { Ionicons } from "@expo/vector-icons";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
 import { Tabs } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { MD3LightTheme as DefaultTheme, PaperProvider } from "react-native-paper";
 
 import { AuthProvider } from "../providers/AuthProvider";
+import { clientPersister } from "../utils/mmkv";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 24 * 60 * 60 * 1000, // 24 hours
+    },
+  },
+});
+
+persistQueryClient({
+  queryClient,
+  persister: clientPersister,
+});
 
 const theme = {
   ...DefaultTheme,
--- src/utils/mmkv.ts
+++ src/utils/mmkv.ts
@@ -1,3 +1,19 @@
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 import { MMKV } from "react-native-mmkv";
 
 export const storage = new MMKV();
+
+const clientStorage = {
+  setItem: (key: string, value: string) => {
+    storage.set(key, value);
+  },
+  getItem: (key: string) => {
+    const value = storage.getString(key);
+    return value === undefined ? null : value;
+  },
+  removeItem: (key: string) => {
+    storage.delete(key);
+  },
+};
+
+export const clientPersister = createSyncStoragePersister({ storage: clientStorage });
